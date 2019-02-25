a13aa2e434c7b6cb64563f1bb8b929b4ec6c8237
Parkstud <parkstud@qq.com>
2019-02-25 23:04:50 +0800

    修改 测试Jenkins自动构建2

--- src/main/java/com/example/springboot1/controller/HelloController.java
+++ src/main/java/com/example/springboot1/controller/HelloController.java
@@ -19,6 +19,6 @@ import java.util.Date;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "测试 Jenkins自动构建"+new Date();
+        return "测试 Jenkins自动构建2"+new Date();
     }
 }
